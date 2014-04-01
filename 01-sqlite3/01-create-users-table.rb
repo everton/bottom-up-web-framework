3eda5cf6d4aa4769efd46a8ea5de4c4ca03c08c6
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-01 00:34:04 -0300

    Simplify sqlite create first (users) table example

--- 01-sqlite3/01-create-users-table.rb
+++ 01-sqlite3/01-create-users-table.rb
@@ -4,22 +4,10 @@ require 'sqlite3'
 
 db = SQLite3::Database.new '01-sqlite3/db/development.sql'
 
-create_sql = %q{
+db.execute %q{
   CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     login VARCHAR(255) NOT NULL,
     password VARCHAR(255)
   );
 }
-
-count  = db.execute create_sql
-
-tables = db.execute 'SELECT tbl_name, sql FROM sqlite_master WHERE type="table";'
-
-tables.each do |table|
-  table_name = table[0]
-  schema_sql = table[1]
-
-  puts "Table: #{table_name}"
-  puts "SQL:   #{schema_sql}"
-end
