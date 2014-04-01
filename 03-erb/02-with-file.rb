446208cec3e3d5b8c413a6e98f4fdc1313e1fab9
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-01 16:49:06 -0300

    Add example of ERB with files and also using methods

---
+++ 03-erb/02-with-file.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+
+@title = 'Sample page created using ERB'
+
+def l337(text)
+  text.tr 'aeiost', '431057'
+end
+
+filepath = File.join File.dirname(__FILE__),
+  'template-example.html.erb'
+
+template = ERB.new File.read(filepath)
+
+puts template.run(binding)
