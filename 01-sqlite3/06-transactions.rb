ec93e6e24b693a6444fa7c217716bbbae0abe0cd
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-03 01:31:24 -0300

    Improve transactions example

--- 01-sqlite3/06-transactions.rb
+++ 01-sqlite3/06-transactions.rb
@@ -11,6 +11,10 @@ puts "There is #{count} users on database now"
 begin
   db.transaction do |transaction|
     db.execute "INSERT INTO users (login) VALUES ('New User');"
+
+    count = db.get_first_value('SELECT COUNT(*) FROM users;')
+    puts "There is #{count} users on database now"
+
     db.execute "INSERT INTO users (login) VALUES (NULL      ) ;"
   end
 rescue => e
