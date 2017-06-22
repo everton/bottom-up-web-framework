e7d9e70dd98b871216159bff0bf8b0048a9e39c1
Everton J. Carpes <everton.carpes@gmail.com>
2017-06-22 04:08:24 -0300

    Add one more case into SQL injectios example
     - Add case of "malicious paramameter" being bound into placeholder
       (which is not supposed to allow injection)

--- 01-sqlite3/07-sql-injection.rb
+++ 01-sqlite3/07-sql-injection.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
 
-require 'benchmark'
-
 require_relative 'config/app'
+
 db = App.connection
 
 ingenuous_param = "user1"
@@ -11,3 +10,5 @@ malicious_param = "' OR 1=1 --"
 puts db.execute("SELECT * FROM users WHERE login='#{ingenuous_param}';").inspect
 
 puts db.execute("SELECT * FROM users WHERE login='#{malicious_param}';").inspect
+
+puts db.execute("SELECT * FROM users WHERE login=:login;", login: malicious_param).inspect
